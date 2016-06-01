e156da045ed58239ed5ab109516a0fcb271b7b40
choushishi <dengsijian@gmail.com>
2016-06-01 10:09:49 +0800

    add official metrics

--- util.R
+++ util.R
@@ -22,3 +22,24 @@ hashIdMap = function(x) {
     wow = clustermap[(clustermap == x)[,c(2,1)]]
     return(wow)
 }
+
+## Official Evaluation Matrics
+
+metrics = function(predict, response) {
+  
+  columns = c("dist", "date", "ts", "gapO")
+  
+  temp1 = predict[,columns]
+  temp2 = predict[,columns]
+  
+  temp = merge(predict, response, by = c("dist", "date", "ts"))
+  
+  temp = temp[, c(4,5)]
+  
+  zeroValues = grep(0, temp[,2])
+  
+  temp = temp[-zeroValues, ]
+  
+  m = sum(abs(temp[,2] - temp[,1]) / temp[,2]) / (nrow(temp))
+  return(m)s
+}
